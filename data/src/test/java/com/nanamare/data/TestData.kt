9ae7141cada17a1f6e18d3e32f63d2db20c8c258
lucas(Shin-hyeonsung) <cordorshs@gmail.com>
2022-05-21 12:01:29 +0900

    [Main] Add remote key model test

--- data/src/test/java/com/nanamare/data/TestData.kt
+++ data/src/test/java/com/nanamare/data/TestData.kt
@@ -1,6 +1,11 @@
 package com.nanamare.data
 
+import com.nanamare.data.local.db.MovieEntity
+import com.nanamare.data.local.db.MovieRemoteKeyEntity
 import com.nanamare.data.model.*
+import com.nanamare.domain.model.MovieModel
+import com.nanamare.domain.model.MovieRemoteKeyModel
+import java.time.Instant
 
 object TestData {
 
@@ -31,6 +36,56 @@ object TestData {
             results = listOf(movieDto, movieDto),
             totalPages = 1,
             totalResults = 2
+        )
+
+        val movieEntity = MovieEntity(
+            primaryKey = 100,
+            adult = true,
+            backdropPath = "backdropPath",
+            genreIds = listOf(1, 2, 3),
+            id = 50,
+            originalLanguage = "EN",
+            originalTitle = "Matrix",
+            overview = "String recommended",
+            popularity = 9.0,
+            posterPath = "posterPath",
+            releaseDate = "2022-05-21",
+            title = "Matrix",
+            video = true,
+            voteAverage = 7.7,
+            voteCount = 5111
+        )
+
+        val movieModel = MovieModel(
+            primaryKey = 100,
+            adult = true,
+            backdropPath = "backdropPath",
+            genreIds = listOf(1, 2, 3),
+            id = 50,
+            originalLanguage = "EN",
+            originalTitle = "Matrix",
+            overview = "String recommended",
+            popularity = 9.0,
+            posterPath = "posterPath",
+            releaseDate = "2022-05-21",
+            title = "Matrix",
+            video = true,
+            voteAverage = 7.7,
+            voteCount = 5111
+        )
+
+        val movieRemoteKeyEntity = MovieRemoteKeyEntity(
+            id = 7,
+            prevPage = 1,
+            nextPage = 3,
+            lastUpdated = Instant.EPOCH.epochSecond
+        )
+
+        val movieRemoteKeyModel = MovieRemoteKeyModel(
+            id = 7,
+            prevPage = 1,
+            nextPage = 3,
+            lastUpdated = Instant.EPOCH.epochSecond
         )
     }
 
--- data/src/test/java/com/nanamare/data/model/MovieTest.kt
+++ data/src/test/java/com/nanamare/data/model/MovieTest.kt
@@ -2,11 +2,16 @@ package com.nanamare.data.model
 
 import com.google.common.truth.Truth.assertThat
 import com.nanamare.data.TestData
+import com.nanamare.data.local.db.MovieEntity
+import com.nanamare.data.local.db.MovieRemoteKeyEntity
 import com.nanamare.data.model.mapper.toDomainModel
+import com.nanamare.data.model.mapper.toEntity
 import com.nanamare.domain.model.DatesModel
 import com.nanamare.domain.model.MovieModel
+import com.nanamare.domain.model.MovieRemoteKeyModel
 import com.nanamare.domain.model.MovieResponseModel
 import org.junit.Test
+import java.time.Instant
 
 class MovieTest {
 
@@ -47,6 +52,62 @@ class MovieTest {
         assertThat(movieResponseModel.totalResults).isEqualTo(2)
         assertThat(movieResponseModel.dates).isEqualTo(TestData.Movie.datesDto.toDomainModel())
         assertThat(movieResponseModel.movies[0]).isEqualTo(TestData.Movie.movieDto.toDomainModel())
+    }
+
+    @Test
+    fun movie_entity_can_be_mapped_to_domain_model() {
+        val movieEntity: MovieEntity = TestData.Movie.movieEntity
+
+        val movieDomainModel = movieEntity.toDomainModel()
+
+        assertThat(movieDomainModel.originalTitle).isEqualTo("Matrix")
+        assertThat(movieDomainModel.title).isEqualTo("Matrix")
+        assertThat(movieDomainModel.id).isEqualTo(50)
+        assertThat(movieDomainModel.overview).isEqualTo("String recommended")
+        assertThat(movieDomainModel.genreIds).isEqualTo(listOf(1, 2, 3))
+        assertThat(movieDomainModel.releaseDate).isEqualTo("2022-05-21")
+        assertThat(movieDomainModel.voteAverage).isEqualTo(7.7)
+        assertThat(movieDomainModel.voteCount).isEqualTo(5111)
+    }
+
+    @Test
+    fun movie_domain_can_be_mapped_to_entity() {
+        val movieModel: MovieModel = TestData.Movie.movieModel
+
+        val movieEntity = movieModel.toEntity()
+
+        assertThat(movieEntity.originalTitle).isEqualTo("Matrix")
+        assertThat(movieEntity.title).isEqualTo("Matrix")
+        assertThat(movieEntity.id).isEqualTo(50)
+        assertThat(movieEntity.overview).isEqualTo("String recommended")
+        assertThat(movieEntity.genreIds).isEqualTo(listOf(1, 2, 3))
+        assertThat(movieEntity.releaseDate).isEqualTo("2022-05-21")
+        assertThat(movieEntity.voteAverage).isEqualTo(7.7)
+        assertThat(movieEntity.voteCount).isEqualTo(5111)
+    }
+
+    @Test
+    fun movie_remote_key_entity_can_be_mapped_to_domain_model() {
+        val movieRemoteKeyEntity: MovieRemoteKeyEntity = TestData.Movie.movieRemoteKeyEntity
+
+        val movieRemoteKeyDomainModel = movieRemoteKeyEntity.toDomainModel()
+
+        assertThat(movieRemoteKeyDomainModel.id).isEqualTo(7)
+        assertThat(movieRemoteKeyDomainModel.prevPage).isEqualTo(1)
+        assertThat(movieRemoteKeyDomainModel.nextPage).isEqualTo(3)
+        assertThat(movieRemoteKeyDomainModel.lastUpdated).isEqualTo(Instant.EPOCH.epochSecond)
+    }
+
+    @Test
+    fun movie_remote_key_domain_model_can_be_mapped_to_entity() {
+        val movieRemoteKeyModel: MovieRemoteKeyModel = TestData.Movie.movieRemoteKeyModel
+
+        val movieRemoteKeyEntity = movieRemoteKeyModel.toEntity()
+
+        assertThat(movieRemoteKeyEntity.id).isEqualTo(7)
+        assertThat(movieRemoteKeyEntity.prevPage).isEqualTo(1)
+        assertThat(movieRemoteKeyEntity.nextPage).isEqualTo(3)
+        assertThat(movieRemoteKeyEntity.lastUpdated).isEqualTo(Instant.EPOCH.epochSecond)
     }
 
 }
