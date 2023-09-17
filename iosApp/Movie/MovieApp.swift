5945c90d3871cd7d9f9d4a8970973565be510d4e
HyunWoo Lee <l2hyunwoo@gmail.com>
2023-09-17 19:57:47 +0900

    Add iOS Project in root

---
+++ iosApp/Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by HyunWoo Lee on 2023/09/17.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
